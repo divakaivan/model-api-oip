a0e0d3e6a6b4642eff2900a883cf57fc528c68a7
divakaivan <timee98@outlook.com>
2025-04-24 18:13:50 +0900

    add model info yaml to this stage and update readme

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # 2/endpoints-start
 
+## [FastAPI lifespan](https://fastapi.tiangolo.com/advanced/events/#lifespan)
+
+`You can define logic (code) that should be executed before the application starts up. This means that this code will be executed once, before the application starts receiving requests.` - This is from the FastAPI docs and I suggest you read the whole page. For our ML model, are only interested in loading it once - when the API starts. We don't want our model to be loaded every time a user makes a request. To implement this behaviour we use the FastAPI lifespan events. 
+
+In `main.py` I have given you the skeleton for implementing this lifespan logic. In addition to loading the model at the start, we will also load a `model_info.yaml` which will contain static information about our model like input types, name, version. Some of this information might come from model registries (like MLflow) but using tools like MLflow is outside the scope of what I want to show you, so we will mimic it by creating a yaml file with model information. 
+
+## Endpoints
+
 Let's try to follow the [Open Inference Protocol](https://kserve.github.io/website/latest/modelserving/data_plane/v2_protocol/#httprest) (OIP).
 
 We'll start by deleting the `/predict` endpoint and implement the endpoints from the OIP one by one.
@@ -41,7 +49,7 @@ def server_ready():
     return JSONResponse(status_code=status.HTTP_200_OK, content=response.model_dump())
 ```
 
-If you run `python main.py` now, and go to `http://0.0.0.0:8000/docs` you will see the endpoint and be able to query it.
+If you run `python main.py` now (you may need to comment out the code that's not implemented yet), and go to `http://0.0.0.0:8000/docs` you will see the endpoint and be able to query it.
 
 ![server_ready](example.png)
 
@@ -49,6 +57,7 @@ In the image, you can see the endpoint description appearing in the docs. And be
 
 ![schemas_example](schemas_ex.png)
 
+
 --------------------
 
-Now, I invite you to try to implement at least some (if not all) the other endpoints following the OIP. If you ever get stuck, please reference the code in the `2/endpoints-completed` branch.
+Now, I invite you to try to implement at least some (if not all) the other endpoints following the OIP and the signatures of models and endpoints I have given you. If you ever get stuck, please reference the code in the `2/endpoints-completed` branch. I suggest reading the FastAPI or Pydantic docs if you ever get stuck and don't want to peak at my implementation.
--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field
+from contextlib import asynccontextmanager
+import yaml
+
+ml_models = {}
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    import joblib
+
+    with open("src/model_info.yaml", "r") as f:
+        model_info = yaml.safe_load(f)
+
+    ml_models["latest_model_info"] = model_info
+    ml_models["latest_model"] = joblib.load(model_info["model_path"])
+
+    # above yield are ran on startup
+    yield
+    # below yield are ran on shutdown
+
+    ml_models.clear()
 
 app = FastAPI()
+
+
+############################################
+######### /v2/health/ready #################
+############################################
 
 class ServerReadyResponse(BaseModel):
     ready: bool = Field(
@@ -17,6 +42,134 @@ class ServerReadyResponse(BaseModel):
 def server_ready():
     response = ServerReadyResponse(ready=True)
     return JSONResponse(status_code=status.HTTP_200_OK, content=response.model_dump())
+
+#########################################
+######### /v2/health/live ###############
+#########################################
+
+class ServerLiveResponse(BaseModel):
+    ...
+
+
+@app.get(
+    "/v2/health/live",
+    response_model=ServerLiveResponse,
+    tags=["server"],
+    description="The “server live” health API indicates if the inference server is able to receive and respond to metadata and inference requests.",
+)
+def server_live():
+    ...
+
+
+#############################################################
+######### /v2/models/petal_classifier/v1/ready ##############
+#############################################################
+
+class ModelReadyResponse(BaseModel):
+    ...
+
+
+@app.get(
+    "/v2/models/petal_classifier/v1/ready",
+    response_model=ModelReadyResponse,
+    tags=["model"],
+    description="The “model ready” health API indicates if a specific model is ready for inferencing. The model name and (optionally) version must be available in the URL.",
+)
+def model_ready():
+    ...
+
+
+############################
+######### /v2 ##############
+############################
+
+class ServerMetadataResponse(BaseModel):
+    ...
+
+class ServerMetadataErrorResponse(BaseModel):
+    ...
+
+
+@app.get(
+    "/v2",
+    response_model=ServerMetadataResponse,
+    responses={
+        400: {
+            "model": ServerMetadataErrorResponse,
+            "description": "Error in server metadata request",
+        }
+    },
+    tags=["server"],
+    description="The 'server metadata' API returns details describing the server.",
+)
+def server_metadata():
+    ...
+
+#######################################################
+######### /v2/models/petal_classifier/v1 ##############
+#######################################################
+
+class MetadataTensor(BaseModel):
+    ...
+
+class ModelMetadataResponse(BaseModel):
+    ...
+
+
+class ModelMetadataErrorResponse(BaseModel):
+    ...
+
+
+@app.get(
+    "/v2/models/petal_classifier/v1",
+    response_model=ModelMetadataResponse,
+    responses={
+        400: {
+            "model": ModelMetadataErrorResponse,
+            "description": "Error in model metadata request",
+        }
+    },
+    tags=["model"],
+    description="The 'model metadata' API is a per-model endpoint that returns details about the model passed in the path.",
+)
+def model_metadata():
+    ...
+
+######################################################
+######### /v2/models/petal_classifier/v1/infer #######
+######################################################
+
+class ModelInput(BaseModel):
+    ...
+
+class InferenceRequestInput(BaseModel):
+    ...
+
+class ModelOutput(BaseModel):
+    ...
+
+
+class InferenceRequestOutput(BaseModel):
+    ...
+
+
+class InferenceErrorResponse(BaseModel):
+    ...
+
+@app.post(
+    "/v2/models/petal_classifier/v1/infer",
+    response_model=InferenceRequestOutput,
+    responses={
+        400: {
+            "model": InferenceErrorResponse,
+            "description": "Error in model inference request",
+        }
+    },
+    tags=["model"],
+    description="The /infer endpoint performs inference on a model. The response is the prediction result.",
+)
+def inference(model_input: InferenceRequestInput):
+    ...
 
 
 if __name__ == "__main__":
